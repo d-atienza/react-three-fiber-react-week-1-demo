5baf43e419835339c24a26e4942a1eb43bada5fe
nbogie <neillbogie@googlemail.com>
2024-03-18 13:49:06 +0000

    tweak for readability

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,20 +16,23 @@ function App() {
                 {/* Red = x, Green = y, Blue = z.  Out from the centre is positive.  */}
                 <axesHelper scale={2} position={[0, 1, 0]} />
 
-                {/* A grid with unit markers, on the x-z plane.  */}
+                {/* A grid with unit markers, on the x-z plane (ground plane).  */}
                 <gridHelper position={[0, 0, 0]} />
 
                 {/* A white light that shines from the top front*/}
                 <pointLight position={[10, 10, 10]} />
+                {/* and a blue light, too */}
                 <pointLight position={[-3, 2, -3]} color={"blue"} />
 
-                {/* a box you can click on */}
                 <SimpleBox pos={[3, 0.5, 2]} />
+
+                <ChristmasTree pos={[-2, 0, -2]} />
+                <ChristmasTree pos={[3, 0, 0]} />
+
+                {/* some boxes that you can click on */}
                 <SpinnyBox pos={[1, 0.5, -5]} />
                 <SpinnyBox pos={[3, 4, -2]} />
                 <SpinnyBox pos={[-4, -3, 3]} />
-                <ChristmasTree pos={[-2, 0, -2]} />
-                <ChristmasTree pos={[3, 0, 0]} />
             </Canvas>
         </div>
     );
